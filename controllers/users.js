eb039b63eb8ec65887914cc87ca8e50b73350441
SenpaiSun <www.maxxi@mail.ru>
2023-07-01 13:16:42 +0600

    Corrected the value of the models.

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,9 @@ module.exports.getUsers = (req, res) => {
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then((user) => res.send({ user }))
+    .then((user) => {
+      res.send({ user });
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         return res.status(BAD_REQUEST).send({
