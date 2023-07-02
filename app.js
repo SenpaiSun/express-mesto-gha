f111eeb91e4451b1cf6ae38e41e3e86ceeba1033
SenpaiSun <www.maxxi@mail.ru>
2023-07-02 14:17:39 +0600

    install rate limit and helmet

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { NOT_FOUND } = require('http-status-codes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cardsRouter = require('./routes/cards');
 const usersRouter = require('./routes/users');
 
 const app = express();
 const { PORT = 3000 } = process.env;
-
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+});
 mongoose.connect('mongodb://localhost:27017/mestodb', { family: 4 });
 
+app.use(limiter);
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
